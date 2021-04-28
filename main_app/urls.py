b3947c242a89ff1e207e24c8c87f3424980bd9e6
chuckchoiboi <hyunchoi94@gmail.com>
2021-04-27 19:18:26 -0700

    add upcoming routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # photos
+    # path('photos/<int:photo_id>/', views.photos_detail, name='detail'),
+    # path('photos/create/', views.photos_create, name='photos_create'),
+    # path('photos/<int:pk>/update/', views.photos_update, name='photos_update'),
+    # path('photos/<int:pk>/delete/', views.photos_delete, name='photos_delete'),
+
+    # account urls
+    # path('accounts/signup/', views.signup, name='signup'),
 ]
