fc0f936d6d8890e59fa06b70ab0163061c42beee
chuckchoiboi <hyunchoi94@gmail.com>
2021-04-28 19:48:56 -0700

    add photo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Photo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
+    title = models.CharField(max_length=150)
+    privacy = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("photo_detail", kwargs={"photo_id": self.pk})
