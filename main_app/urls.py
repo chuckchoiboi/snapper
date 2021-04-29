789b64b1649fec2aecb154745bfdcbecb731eea9
chuckchoiboi <hyunchoi94@gmail.com>
2021-04-28 20:11:32 -0700

    add photos model view

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     # photos
-    # path('photos/<int:photo_id>/', views.photos_detail, name='detail'),
+    path('photos/<int:photo_id>/', views.photos_detail, name='detail'),
     # path('photos/create/', views.photos_create, name='photos_create'),
     # path('photos/<int:pk>/update/', views.photos_update, name='photos_update'),
     # path('photos/<int:pk>/delete/', views.photos_delete, name='photos_delete'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Photo
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,21 +10,9 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'photos': photos})
 
-
-class Photo:
-    def __init__(self, author, url, title, privacy, interactions):
-        self.author = author
-        self.url = url
-        self.title = title
-        self.privacy = privacy
-        self.interactions = interactions
+# photos view
 
 
-photos = [
-    Photo('chuckchoiboi', 'https://user-images.githubusercontent.com/60675322/116333406-3adec180-a788-11eb-98ab-afdfbf6701b0.JPG',
-          'Image 1', False, ['chuckchoiboi']),
-    Photo('chuckchoiboi', 'https://user-images.githubusercontent.com/60675322/116333411-3ca88500-a788-11eb-8cb2-e4a7068ade84.JPG',
-          'Image 2', False, ['chuckchoiboi']),
-    Photo('chuckchoiboi', 'https://user-images.githubusercontent.com/60675322/116333416-3e724880-a788-11eb-8f60-f1f8b53a9d31.JPG',
-          'Image 3', False, ['chuckchoiboi']),
-]
+def photos_detail(request, photo_id):
+    photo = Photo.objects.get(id=photo_id)
+    return render(request, 'photos/detail.html', {'photo': photo})
